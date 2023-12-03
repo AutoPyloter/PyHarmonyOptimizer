73baba29129c1ec16b1a7c1863a8d05f6590c2da
Abdulkadir Özcan <151790206+AutoPyloter@users.noreply.github.com>
2023-12-03 15:25:39 +0300

    Create pyharmony.py

---
+++ pyharmony.py
@@ -0,0 +1,60 @@
+import random
+
+class Continuous:
+    def __init__(self, min_val, max_val):
+        self.min_val = min_val
+        self.max_val = max_val
+
+    def sample(self):
+        return random.uniform(self.min_val, self.max_val)
+
+class Discrete:
+    def __init__(self, values):
+        self.values = values
+
+    def sample(self):
+        return random.choice(self.values)
+
+class Categorical:
+    def __init__(self, categories):
+        self.categories = categories
+
+    def sample(self):
+        return random.choice(self.categories)
+
+class Minimization:
+    def __init__(self, design, objective):
+        self.design = design
+        self.objective = objective
+        self.harmony_memory = []
+
+    def initialize_harmony_memory(self, size):
+        for _ in range(size):
+            harmony = {var: self.design[var].sample() for var in self.design}
+            self.harmony_memory.append((harmony, self.objective(harmony)))
+
+    def generate_new_harmony(self, HMCR, PAR):
+        new_harmony = {}
+        for var in self.design:
+            if random.random() < HMCR:
+                new_harmony[var] = random.choice([h[0][var] for h in self.harmony_memory])
+            else:
+                new_harmony[var] = self.design[var].sample()
+
+            if random.random() < PAR:
+                new_harmony[var] = self.design[var].sample()
+
+        return new_harmony
+
+    def optimize(self, HMCR, PAR, memory_size, max_iter):
+        self.initialize_harmony_memory(memory_size)
+        for _ in range(max_iter):
+            new_harmony = self.generate_new_harmony(HMCR, PAR)
+            new_fitness = self.objective(new_harmony)
+
+            worst_harmony = max(self.harmony_memory, key=lambda x: x[1])
+            if worst_harmony[1] > new_fitness:
+                self.harmony_memory.remove(worst_harmony)
+                self.harmony_memory.append((new_harmony, new_fitness))
+
+        return min(self.harmony_memory, key=lambda x: x[1])
